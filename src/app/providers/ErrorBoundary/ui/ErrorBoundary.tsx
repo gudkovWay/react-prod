fab1245ba80059468afa338be8c53e3942dfeb45
gudkovWay <gudkoviurii@gmail.com>
2024-08-15 02:34:04 +0300

    Error Boundary

---
+++ src/app/providers/ErrorBoundary/ui/ErrorBoundary.tsx
@@ -0,0 +1,41 @@
+import React, { ErrorInfo, Suspense } from "react";
+import { PageError } from "widgets/PageError";
+import { PageLoader } from "widgets/PageLoader";
+
+interface ErrorBoundaryProps {
+  children: React.ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends React.Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error: Error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, info: ErrorInfo) {
+    console.log(error, info.componentStack);
+  }
+
+  render() {
+    const { hasError } = this.state;
+    const { children } = this.props;
+
+    if (hasError) {
+      return <Suspense fallback={<PageLoader />}><PageError /></Suspense>;
+    }
+
+    return children;
+  }
+}
+export default ErrorBoundary;
--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,17 @@ import { ThemeProvider } from "app/providers/ThemeProvider";
 import App from "app/App";
 
 import "shared/config/i18n/i18n";
+import { ErrorBoundary } from "app/providers/ErrorBoundary";
 
 const rootElement = document.getElementById("root");
 
 const AppWithProviders = () => (
   <BrowserRouter>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <ErrorBoundary>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </ErrorBoundary>
   </BrowserRouter>
 );
 
