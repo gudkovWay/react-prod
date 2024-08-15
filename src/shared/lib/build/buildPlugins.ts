623e3733502079a92d3d06836f62d314ba864a1e
gudkovWay <gudkoviurii@gmail.com>
2024-08-15 18:47:05 +0300

    🤌 bundle analyzer

--- src/shared/lib/build/buildPlugins.ts
+++ src/shared/lib/build/buildPlugins.ts
@@ -2,11 +2,12 @@ import webpack from "webpack";
 import HTMLWebpackPlugin from "html-webpack-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
+import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 
 import { BuildOptions } from "./types/config";
 
 export function buildPlugins(
-  options: BuildOptions,
+  options: BuildOptions
 ): webpack.WebpackPluginInstance[] {
   const { paths, isDev } = options;
   const plugins = [
@@ -21,6 +22,7 @@ export function buildPlugins(
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
     }),
+    new BundleAnalyzerPlugin({ openAnalyzer: false }),
   ];
   if (isDev) {
     plugins.push(new ReactRefreshWebpackPlugin());
